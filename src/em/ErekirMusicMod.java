77dc89538e7ef04185d2d4080f2f7c5e512954e5
TheSlaylord <abevnyaev@gmail.com>
2023-06-22 09:10:40 +0300

    Version 1.0.2 aka Not So Quik Fix I actually tested this one this time.

--- src/em/ErekirMusicMod.java
+++ src/em/ErekirMusicMod.java
@@ -3,12 +3,14 @@ package em;
 import arc.Events;
 import arc.audio.Music;
 import arc.struct.Seq;
+import arc.util.Log;
 import em.content.EMusic;
 import mindustry.Vars;
 import mindustry.audio.SoundControl;
 import mindustry.content.Planets;
-import mindustry.game.EventType.*;
 import mindustry.mod.Mod;
+
+import static mindustry.game.EventType.*;
 
 public class ErekirMusicMod extends Mod {
     /** List of ambient music. */
@@ -19,50 +21,52 @@ public class ErekirMusicMod extends Mod {
     public Seq<Music> modBoss = new Seq<>();
 
     /** List of <i>vanilla</i> ambient music. */
-    public Seq<Music> vAmbient = new Seq<>();
+    public Seq<Music> vAmbient;
     /** List of <i>vanilla</i> dark music. */
-    public Seq<Music> vDark = new Seq<>();
+    public Seq<Music> vDark;
     /** List of <i>vanilla</i> boss music. */
-    public Seq<Music> vBoss = new Seq<>();
+    public Seq<Music> vBoss;
 
     /** A reference to SoundControl instance for easier access. */
-    public SoundControl control = Vars.control.sound;
+    public SoundControl control;
 
-    public ErekirMusicMod() {
-        // Initiate music lists here.
-        Events.on(ClientLoadEvent.class, e -> {
-            var universal = Seq.with(EMusic.dosimeter, EMusic.infernalTrain, EMusic.wotu);
-            modAmbient.addAll(universal);
-            modDark.addAll(universal);
-            modBoss.addAll();
-
-            vAmbient = control.ambientMusic;
-            vDark = control.darkMusic;
-            vBoss = control.bossMusic;
-        });
-
-        // Inject custom music here.
-        Events.on(WorldLoadEvent.class, e -> {
-            if (Vars.state.rules.planet != Planets.erekir) return;
-
-            control.ambientMusic = modAmbient;
-            control.darkMusic = modDark;
-            //TODO: this will just erase all the boss music since the list is currently empty
-            //control.bossMusic = modBoss;
-        });
-
-        // Reset music here.
-        Events.on(StateChangeEvent.class, e -> {
-            if (Vars.state.rules.planet != Planets.erekir) return;
-
-            control.ambientMusic = vAmbient;
-            control.darkMusic = vDark;
-            control.bossMusic = vBoss;
-        });
-    }
+    public ErekirMusicMod() {}
 
     @Override
-    public void loadContent(){
+    public void init() {
+        // First and foremost, load the music.
         EMusic.load();
+
+        // Initiate music lists here.
+        control = Vars.control.sound;
+
+        var universal = Seq.with(EMusic.dosimeter, EMusic.infernalTrain, EMusic.wotu);
+        modAmbient.addAll(universal);
+        modDark.addAll(universal);
+        modBoss.addAll();
+
+        Events.on(MusicRegisterEvent.class, e -> {
+            vAmbient = control.ambientMusic.copy();
+            vDark = control.darkMusic.copy();
+            vBoss = control.bossMusic.copy();
+        });
+
+        Events.on(WorldLoadEvent.class, e -> {
+            if (Vars.state.rules.planet == Planets.erekir) {
+                // Inject custom music here.
+                Log.info("The music should load now...");
+
+                control.ambientMusic = modAmbient;
+                control.darkMusic = modDark;
+                //TODO: this will just erase all the boss music since the list is currently empty
+                //control.bossMusic = modBoss;
+            } else {
+                // Reset music in non-Erekir maps.
+                Log.info("Music reset. It should be like vanilla now.");
+                control.ambientMusic = vAmbient;
+                control.darkMusic = vDark;
+                control.bossMusic = vBoss;
+            }
+        });
     }
 }
